fae53a73dc157837f0288745fb64088c337b6e8e
Cameron Bytheway <bytheway.cameron@gmail.com>
2024-10-29 14:24:20 -0600

    add swc plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,23 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
+import swc from "@rollup/plugin-swc";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+const { NODE_ENV } = process.env;
+const IS_PROD = NODE_ENV == "production";
+
+const plugins = [
+  nodeResolve({
+    browser: true,
+    mainFields: ["module", "main"],
+  }),
+  commonjs(),
+  json(),
+];
+
+if (IS_PROD) {
+  plugins.push(swc());
+}
 
 export default {
   input: "lib/index.js",
@@ -8,15 +25,9 @@ export default {
     dir: "target/book-src/js",
     format: "iife",
     sourcemap: true,
-          // TODO fix path
-    sourcemapBaseUrl: "http://localhost:3000/js",
+    sourcemapBaseUrl: IS_PROD
+      ? "https://camshaft.github.io/kew/js"
+      : "http://localhost:300/js",
   },
-  plugins: [
-    nodeResolve({
-      browser: true,
-      mainFields: ["module", "main"],
-    }),
-    commonjs(),
-    json(),
-  ],
+  plugins,
 };
